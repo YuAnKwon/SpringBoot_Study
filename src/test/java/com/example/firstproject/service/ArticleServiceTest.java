ca42a4331a0dd75ecc3014d7d985734a6e6260e6
YuAnKwon <dreamhunt17@gmail.com>
2025-03-23 17:33:10 +0900

    25.03.23

--- src/test/java/com/example/firstproject/service/ArticleServiceTest.java
+++ src/test/java/com/example/firstproject/service/ArticleServiceTest.java
@@ -1,9 +1,11 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.ArticleForm;
 import com.example.firstproject.entity.Article;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -34,17 +36,115 @@ class ArticleServiceTest {
     @Test
     void show_성공_존재하는_id_입력() {
         //1. 예상 데이터
-
+        Long id = 1L;
+        Article expected = new Article(id, "가가가가", "1111");
         //2. 실제 데이터
-
+        Article article = articleService.show(id);
         //3. 데이터 비교해 검증
+        assertEquals(expected.toString(), article.toString());
     }
+
     @Test
-    void show_실패() {
+    void show_실패_존재하지않는_id입력() {
         //1. 예상 데이터
+        Long id = -1L;
+        Article expected = null;
+        //2. 실제 데이터
+        Article article = articleService.show(id);
+        //3. 데이터 비교해 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void create_성공_title과_content만있는_dto입력() {
+        //1. 예상 데이터
+        String title = "라라라라";
+        String content = "4444";
+
+        ArticleForm dto = new ArticleForm(null, title, content); //dto 생성
+        Article expected = new Article(4L, title, content); //예상 데이터 저장
 
         //2. 실제 데이터
-
+        Article article = articleService.create(dto);
         //3. 데이터 비교해 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void create_실패_id가_포함된_dto입력() {
+        //1. 예상 데이터
+        ArticleForm dto = new ArticleForm(4L, "라라라라", "4444");
+        Article expected = null;
+        //2. 실제 데이터
+        Article article = articleService.create(dto);
+        //3. 데이터 비교해 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void update_성공_존재하는id와_title_content가있는_dto입력() {
+        //1. 예상 데이터
+        Long id = 3L;
+        ArticleForm dto = new ArticleForm(id, "가나다라", "1234");
+        Article article = new Article(id, "가나다라","1234");
+        //2. 실제 데이터
+        Article expected = articleService.update(id,dto);
+        //3. 데이터 비교해 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_성공_존재하는id와_title만있는_dto입력() {
+        //1. 예상 데이터
+        Long id = 2L;
+        ArticleForm dto = new ArticleForm(id, "가나다라", null);
+        Article article = new Article(id, "가나다라","2222");
+        //2. 실제 데이터
+        Article expected = articleService.update(id,dto);
+        //3. 데이터 비교해 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void update_실패_존재하지않는_id의_dto입력() {
+        //1. 예상 데이터
+        Long id = -1L;
+        ArticleForm dto = new ArticleForm(id, "가나다라", "1234");
+        Article article = null;
+        //2. 실제 데이터
+        Article expected = articleService.update(id,dto);
+        //3. 데이터 비교해 검증
+        assertEquals(expected, article);
+    }
+
+    @Test
+    @Transactional
+    void delete_성공_존재하는id_입력() {
+        //1. 예상 데이터
+        Long id = 3L;
+        String title = "다다다다";
+        String content = "3333";
+        Article article = new Article(id, title, content);
+        //2. 실제 데이터
+        Article expected = articleService.delete(id);
+        //3. 데이터 비교해 검증
+        assertEquals(expected.toString(), article.toString());
+    }
+
+    @Test
+    @Transactional
+    void delete_실패_존재하지_않는_id입력() {
+        //1. 예상 데이터
+        Long id = -1L;
+        Article article = null;
+        //2. 실제 데이터
+        Article expected = articleService.delete(id);
+        //3. 데이터 비교해 검증
+        assertEquals(article, expected);
     }
 }
